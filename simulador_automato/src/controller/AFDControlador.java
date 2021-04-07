87e1f615f6a46387f1f47c1639f761998bae4420
lolocah <duartedelira@live.com>
2021-04-06 21:19:19 -0300

    Criação de escopo do projeto

---
+++ simulador_automato/src/controller/AFDControlador.java
@@ -0,0 +1,10 @@
+package controller;
+
+import model.AFD;
+import java.util.List;
+import java.util.ArrayList;
+
+public class AFDControlador {
+    //repositorioAFD é uma lista com todos os estados de um autômato
+    List <AFD> repositorioAFD = new ArrayList();
+}
